45688b48960072c95ea657e44a833461865fee09
Aaron Toponce <aaron.toponce@gmail.com>
2015-03-06 16:14:15 -0700

    back to the better locker

--- lock.sh
+++ lock.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # apt-get install scrot imagemagick i3lock
-#cd ~/Private/screenshots/
-#scrot screenshot.png
-#convert -scale 10% -scale 1000% screenshot.png blur.png
-#shred screenshot.png
-#rm screenshot.png
-#i3lock -i blur.png
-xscreensaver-command -lock
+cd ~/Private/screenshots/
+scrot screenshot.png
+convert -scale 10% -scale 1000% screenshot.png blur.png
+shred screenshot.png
+rm screenshot.png
+i3lock -i blur.png
