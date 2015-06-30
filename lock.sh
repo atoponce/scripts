8b7fd4323923151732496c6699c32927585986aa
Aaron Toponce <aaron.toponce@gmail.com>
2015-06-30 08:32:17 -0600

    add an additional reseed for the kernel by reversing the SHA256SUMS file with tac(1)

--- lock.sh
+++ lock.sh
@@ -6,6 +6,7 @@ sha256sum screenshot.png >> SHA256SUMS
 convert -scale 10% -scale 1000% screenshot.png blur.png
 sha256sum blur.png >> SHA256SUMS
 sha512sum SHA256SUMS > /dev/urandom
+tac SHA256SUMS | sha512sum /dev/stdin > /dev/urandom
 shred screenshot.png
 rm screenshot.png
 i3lock -i blur.png
