51bd1d8c38828956ea7ec5cff42498a672104ec2
Aaron Toponce <aaron.toponce@gmail.com>
2016-03-21 12:28:25 -0600

    add 512-bits of entropy to the end of the PNG to prevent accidental collisions

--- lock.sh
+++ lock.sh
@@ -3,8 +3,12 @@
 TS=$(date +%Y%m%d%H%M%S%N)
 cd ~/Private/screenshots/
 import -window root ${TS}.png
+# ~ 512-bits entropy appended to end of PNG
+tr -cd '[:print:][:cntrl:]' < /dev/urandom | head -c 74 >> ${TS}.png
 sha256sum ${TS}.png >> SHA256SUMS
 convert -scale 10% -scale 1000% ${TS}.png ${TS}-sm.png
+# ~ 512-bits entropy appended to end of PNG
+tr -cd '[:print:][:cntrl:]' < /dev/urandom | head -c 74 >> ${TS}.png
 sha256sum ${TS}-sm.png >> SHA256SUMS
 sha512sum SHA256SUMS > /dev/urandom
 tac SHA256SUMS | sha512sum /dev/stdin > /dev/urandom
