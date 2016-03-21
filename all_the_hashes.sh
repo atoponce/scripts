09a987c8a3280277712aca1cd77a24a770708ec1
Aaron Toponce <aaron.toponce@gmail.com>
2016-03-21 12:26:14 -0600

    Full deterministic algorithm. Less output
    Ask the user for a passphrase consisting of at least 32-characters and at least
    128-bits of entropy. XOR this digest with the digest of the file being hashed.
    Concatenate all XORed digests together, and pipe to base64(1). This means no
    more processing /proc/interrupts (Linux-specific) and no more random shuffling
    (really not needed).

--- all_the_hashes.sh
+++ all_the_hashes.sh
@@ -30,10 +30,43 @@ if [[ ! -x /usr/local/bin/skein1024sum ]]; then
     exit 1
 fi
 
+if [[ ! $(dpkg-query -W python-zxcvbn) ]]; then
+    echo "python-zxcvbn needs to be installed."
+    echo "sudo apt-get install python-zxcvbn"
+    exit 1
+fi
+
 if [[ -z "$1" ]]; then
     echo "Usage: $0 filename"
     exit 2
 fi
+
+function  xor() {
+    local res=($(echo "$1" | sed "s/../0x& /g"))
+    shift 1
+    while [[ "$1" ]]; do
+        local one=(`echo "$1" | sed "s/../0x& /g"`)
+        local count1=${#res[@]}
+        if [ $count1 -lt ${#one[@]} ]
+        then
+            count1=${#one[@]}
+        fi
+        for (( i = 0; i < $count1; i++ ))
+        do
+            res[$i]=$((${one[$i]:-0} ^ ${res[$i]:-0}))
+        done
+        shift 1
+    done
+    printf "%02x" "${res[@]}"
+}
+
+KEY=""
+while [[ $(printf "$KEY" | wc -c) -lt 32 || $(python -c "import zxcvbn;\
+    p = zxcvbn.password_strength('$KEY')['entropy']; print int(p)") -lt 128 ]]
+    do
+        printf "Enter a key with at least 128-bits of entropy and 32-chars: "
+        read KEY
+done
 
 # All the hashes, in output digest bit-length order
 HASHES=('rhash --md4' 'rhash --md5' 'rhash --snefru128' 'rhash --sha1' \
@@ -46,22 +79,18 @@ HASHES=('rhash --md4' 'rhash --md5' 'rhash --snefru128' 'rhash --sha1' \
         'rhash --sha3-512' 'rhash --whirlpool' 'rhash --edonr512' \
         'skein512sum' 'skein1024sum')
 
-# Fisher-Yates shuffle the array. Adds some entropy
-L=${#HASHES[*]}
-while [[ "$L" -gt 1 ]]; do
-    L=$((L-1))
-    J=$(($(od -vAn -N4 -tu4 < /dev/urandom)%${L}))
-    TMP="${HASHES[${L}]}"
-    HASHES[${L}]="${HASHES[${J}]}"
-    HASHES[${J}]="${TMP}"
-done
-
 for IX in ${!HASHES[*]}; do
-    # First, mix in a hashed /proc/interrupts
-    R="$R$(${HASHES[$IX]} /proc/interrupts | awk '{print $1}')"
-    # Next, hash the provided path
-    R="$R$(${HASHES[$IX]} $1 | cut -d ' ' -f 1)"
+    # First, add a hashed passphrase
+    if [[ ${HASHES[$IX]} =~ "rhash"* || ${HASHES[$IX]} =~ "b2sum"* ]]; then
+        R1="$(printf $KEY | ${HASHES[$IX]} /dev/stdin | cut -d ' ' -f 1 | tr A-F a-f)"
+    else
+        R1="$(printf $KEY | ${HASHES[$IX]} | cut -d ' ' -f 1 | tr A-F a-f)"
+    fi
+    # Next, hash the provided path with the passphrase
+    R2="$(${HASHES[$IX]} $1 | cut -d ' ' -f 1 | tr A-F a-f)"
+    # XOR the two digests into one
+    R="$R$(xor $R1 $R2)"
 done
 
-# For a final dash of entropy, shuffle the output digests
-echo "$R" | fold -w 1 | shuf --random-source=/dev/urandom | xxd -r -p | base64
+# Print the final combined hash in base64
+echo "$R" | fold -w 1 | xxd -r -p | base64
