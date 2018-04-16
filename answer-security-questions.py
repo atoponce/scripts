f4c086295195214a29c0e0c88b98d6f620ebaf1d
Aaron Toponce <aaron.toponce@gmail.com>
2018-04-16 11:35:53 -0600

    minor updates, including parameters

--- answer-security-questions.py
+++ answer-security-questions.py
@@ -6,9 +6,9 @@
 # guaranteeing a minimum security margin to prevent anyone who knows the actual
 # answer from compromising an account.
 
-# SHA-3 is based on Keccak which uses the sponge construction
-# bcrypt is based on Blowfish which uses the feistel construction
 # BLAKE2 is based on ChaCha and uses the HAIFA construction
+# SHAKE256 is a SHA-3 XOF based on Keccak which uses the sponge construction
+# scrypt is a KDF based on PBKDF2-HMAC-SHA256, which uses the Merkle-Damgard construction
 
 from Cryptodome.Hash import BLAKE2b
 from Cryptodome.Hash import SHAKE256
@@ -20,8 +20,8 @@ key = raw_input("What is a secret key with at least 128-bits entropy only you kn
 answer = raw_input("What is the answer to the security question? ")
 
 shake = SHAKE256.new()
-secret = shake.update(bytes(key)).read(32)
-salt = BLAKE2b.new(digest_bits=256, key=(secret))
+secret = shake.update(bytes(key)).read(64)
+salt = BLAKE2b.new(digest_bits=512, key=secret)
 salt.update(bytes(site))
 
 # A cost of 32 MiB of RAM and 5+ seconds computation
