66753da598460d2e170d2d0ac88ce1e558772416
Aaron Toponce <aaron.toponce@gmail.com>
2018-04-15 21:42:01 -0600

    fix spurious output when executing, reorder hashes

--- pwhash.sh
+++ pwhash.sh
@@ -3,8 +3,8 @@
 # Just a stupid script for generating some simple password hashes that are
 # common among various *NIX systems and daemons. Public domain.
 
-command -v htpasswd &>/dev/random || { echo >&2 "htpasswd not installed."; exit 1; }
-command -v mkpasswd &>/dev/random || { echo >&2 "mkpasswd not installed."; exit 2; }
+command -v htpasswd 2>&1 >/dev/random || { echo >&2 "htpasswd not installed."; exit 1; }
+command -v mkpasswd 2>&1 >/dev/random || { echo >&2 "mkpasswd not installed."; exit 2; }
 
 read -p "Enter password: " pw
 echo # blank line
@@ -14,6 +14,6 @@ echo "      MySQL: $(printf "$pw" | sha1sum | awk '{print $1}' | xxd -r -p |
                      sha1sum | sed 's/^/*/g' | awk '{print $1}' | tr a-f A-F)"
 echo "   descrypt: $(echo "$pw" | mkpasswd -P 0)"
 echo "   md5crypt: $(echo "$pw" | mkpasswd -m md5 -P 0)"
+echo "     bcrypt: $(htpasswd -bnB '' $pw | tr -d ':\n' | sed 's/^\$..\$/$2b$/g')"
 echo "sha256crypt: $(echo "$pw" | mkpasswd -m sha-256 -P 0)"
 echo "sha512crypt: $(echo "$pw" | mkpasswd -m sha-512 -P 0)"
-echo "     bcrypt: $(htpasswd -bnB '' $pw | tr -d ':\n' | sed 's/^\$..\$/$2b$/g')"
