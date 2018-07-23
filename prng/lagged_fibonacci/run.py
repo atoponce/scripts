f7b28bbd2493e3245a107117d66f9c763343da36
Aaron Toponce <aaron.toponce@gmail.com>
2018-07-23 17:36:49 -0600

    update lagged fibonacci sequence with a decent seed

--- prng/lagged_fibonacci/run.py
+++ prng/lagged_fibonacci/run.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-lfg = [n*41 for n in range(128)]
+lfg = [n**2 % 5011 for n in range(128)]
 j, k, m = 97, 127, 5000
-for i in range(m+1):
+for i in range(m):
     x = (lfg[j] + lfg[k]) % m
-    lfg.insert(0, x)
-    lfg.pop()
+    lfg[k] = x
+    j -= 1
+    k -= 1
+    if j == -1:
+        j = 127
+    if k == -1:
+        k = 127
     print(x/5000)
