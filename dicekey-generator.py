6cd49983a9438be9b2b3b27f21f8889f11331eb6
Aaron Toponce <aaron.toponce@gmail.com>
2020-09-01 12:19:23 -0600

    use the shuffle stupid

--- dicekey-generator.py
+++ dicekey-generator.py
@@ -3,14 +3,11 @@
 import secrets
 
 dice = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","W","X","Y","Z"]
+secrets.SystemRandom().shuffle(dice)
+
 i = 25
 tmp = ""
 pw = ""
-
-while i > 1:
-    i -= 1
-    j = secrets.randbelow(i)
-    dice[j], dice[i] = dice[i], dice[j]
 
 for i in range(25):
     tmp = dice[i]
