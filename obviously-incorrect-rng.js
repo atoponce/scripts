62ac281b9c5eddc58294c89715fe04a742ad9a90
Aaron Toponce <aaron.toponce@gmail.com>
2024-02-09 16:32:42 -0700

    small code tweaks

--- obviously-incorrect-rng.js
+++ obviously-incorrect-rng.js
@@ -52,9 +52,9 @@ function now() {
 function flip_coin() {
   let coin = 0
   let cycles = 0
-  const later = performance.now() + 0.1
+  const later = now() + 0.1
 
-  while(performance.now() <= later) {
+  while(now() <= later) {
     coin ^= 1
     cycles++
   }
@@ -77,7 +77,7 @@ function fair_bit() {
 /**
  * Builds an 8-bit byte, either raw for SHA-256 hashing, or unbiased via von Neumann randomness
  * extraction.
- * @param {string} extractor Either "sha256" or "neumann".
+ * @param {string} extractor Either "raw" or "neumann".
  * @returns {number} Decimal number 0-255.
  */
 function get_byte(extractor) {
@@ -87,7 +87,7 @@ function get_byte(extractor) {
   while(bits--) {
     byte <<= 1
 
-    if(extractor === "sha256")
+    if(extractor === "raw")
       // Raw bits are okay for SHA-256 hashing *after* the fact.
       byte |= flip_coin()[0]
     else if (extractor === "neumann")
@@ -121,7 +121,7 @@ function hash_bytes() {
   let count = 32
   const results = new Uint8Array(count)
 
-  while(count--) results[count] = get_byte("sha256")
+  while(count--) results[count] = get_byte("raw")
 
   return crypto.createHash("sha256").update(results).digest("hex")
 }
@@ -140,7 +140,7 @@ if (require.main === module) {
     else if (args.includes("-c") || args.includes("--coin"))    console.log(flip_coin()[0])
     else if (args.includes("-s") || args.includes("--spins"))   console.log(flip_coin()[1])
     else if (args.includes("-f") || args.includes("--fair"))    console.log(fair_bit())
-    else if (args.includes("-b") || args.includes("--bytes"))   console.log(get_byte("sha256"))
+    else if (args.includes("-b") || args.includes("--bytes"))   console.log(get_byte("raw"))
     else if (args.includes("-v") || args.includes("--neumann")) console.log(get_byte("neumann"))
     else if (args.includes("-2") || args.includes("--sha256"))  console.log(hash_bytes())
     else if (args.includes("-x") || args.includes("--hex"))     console.log(fair_bytes())
