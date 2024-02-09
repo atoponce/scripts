99ab0f96b5fc74efb98f04ab4c688d5214f5b3a6
Aaron Toponce <aaron.toponce@gmail.com>
2024-02-08 22:36:15 -0700

    add commandline options and jsdoc strings

--- obviously-incorrect-rng.js
+++ obviously-incorrect-rng.js
@@ -4,41 +4,64 @@
   * Released to the public domain.
   *
   * This code primarily comes from Dan Kaminsky in 2012. It models coin flips by pitting the RTC
-  * against the CPU. An expiration timer is set 1 millisecond in the future and a bit is flipped
+  * against the CPU. An expiration timer is set at some point in the future and a bit is flipped
   * between 0/1 as fast as possible on the CPU until the timer expires. At timer expiration, the
-  * bit is returned. Because interrupts are not precise, the returned bit a true random bit. 256
-  * bits are collected then hashed with SHA-256 as a randomness extractor. This code is specific
-  * to Node.js. It will not work in the browser.
-  *
-  * Usage:
-  *   $ node obviously-incorrect-rng.js
-  *   e9bbbb8a778094597b6e3b9b1bf49ec874caff16bebe461f5673ff36e00c260a
-  *
-  *   $ node obviously-incorrect-rng.js 5
-  *   27154304541c4cbd7b88c1a77e979d7a8268a466f73d9c8eeccdb701ba704bdb
-  *   dea378433166c1e5bccc2187d92905edadf14773ef35e3f9320ac3e9355274d3
-  *   9bd0a1ad200425ceb4f67d714e6e09ab4586c91fcf6562e8bb5210511b5479a7
-  *   da94e152845656360b69f474db594392ba38b4ccadf530be32e33a2bc68b797c
-  *   d516b875603b9041a6bf49a773e78668fa52b66904a8564925f00f56a924dce5
+  * bit is returned. Because interrupts are not precise, a "true random" bit is generated, after
+  * which it can be sent through a von Neumann randomness extractor or collect 256 bits & hashed
+  * with SHA-256. This code is specific to Node.js. It will not work in the browser.
   */
 
 const crypto = require("crypto")
 
+function print_usage() {
+  console.log("Usage: " + process.argv[1] + " [OPTION [ARG]]\n")
+  console.log("\n")
+  console.log("  -h,   --help      Print this help and exit.\n")
+  console.log("\n")
+  console.log("  -b,   --byte      Get raw bytes.\n")
+  console.log("  -c,   --coin      Get raw coin flips.\n")
+  console.log("\n")
+  console.log("  -f,   --fair      Get fair coin flips (von Neumann extracted).\n")
+  console.log("  -s,   --spins     Get number of spins per coin flip.\n")
+  console.log("  -v,   --neumann   Get fair bytes (von Neumann extracted).\n")
+  console.log("\n")
+  console.log("  -2,   --sha256    Get 256 bits in hex (SHA-256 hashed).\n")
+  console.log("  -x,   --hex       Get 256 bits in hex (von Neumann extracted).\n")
+  console.log("\n")
+  console.log("  -n #, --num #     Print requested data specific number of times.\n")
+}
+
+/**
+ * Returns a high resolution timestamp with sub-millisecond accuracy.
+ * @returns {number}
+ */
+function now() {
+  return performance.now()
+}
+
+/**
+ * Sets a timer in the future and flips a bit between 0/1 until the timer expires.
+ * @returns {number}
+ */
 function flip_coin() {
   let coin = 0
-  const then = performance.now() + 0.1
+  const later = now() + 0.1
 
-  while(performance.now() <= then) coin ^= 1
+  while(now() <= later) coin ^= 1
 
   return coin
 }
 
-function flip_coin_cycles() {
+/**
+ * Counts the number of bit flips before the timer expires.
+ * @returns {Array}
+ */
+function coin_spins() {
   let coin = 0
   let cycles = 0
-  const then = performance.now() + 0.1
+  const later = performance.now() + 0.1
 
-  while(performance.now() <= then) {
+  while(performance.now() <= later) {
     coin ^= 1
     cycles++
   }
@@ -46,53 +69,97 @@ function flip_coin_cycles() {
   return [coin, cycles]
 }
 
-function get_fair_bit() {
+/**
+ * John von Neumann randomness extractor.
+ * @returns {number}
+ */
+function fair_bit() {
   while(1) {
     const a = flip_coin()
     if(a != flip_coin()) return(a)
   }
 }
 
-function get_random_byte() {
+/**
+ * Builds a full byte, either raw (for SHA-256) or after von Neumann randomness extraction.
+ * @param {string} extractor 
+ * @returns {number}
+ */
+function get_byte(extractor) {
   let n = 0
   let bits = 8
 
   while(bits--) {
     n <<= 1
-    n |= flip_coin()
-    //n |= get_fair_bit()
+
+    if(extractor === "sha256")
+      n |= flip_coin()
+    else if (extractor === "neumann")
+      n |= fair_bit()
+    else
+      process.exit(1)
   }
 
   return n
 }
 
-function get_random_hex() {
+/**
+ * Generates a 256-bit hexadecimal string from von Neumann debiased bits.
+ * @returns {string}
+ */
+function fair_bytes() {
   let count = 32
   const results = new Uint8Array(count)
 
-  while(count--) results[count] = get_random_byte()
+  while(count--) results[count] = get_byte("neumann")
 
-  // For debugging/validation.
-  //console.log(results)
+  return Buffer.from(results).toString("hex");
+}
+
+/**
+ * Generates a 256-bit hexadecimal string from SHA-256 hashing.
+ * @returns {string}
+ */
+function hash_bytes() {
+  let count = 32
+  const results = new Uint8Array(count)
+
+  while(count--) results[count] = get_byte("sha256")
 
   return crypto.createHash("sha256").update(results).digest("hex")
 }
 
 if (require.main === module) {
   let count = 1
+  const args = process.argv.slice(2)
   
-  if (process.argv.length > 2) {
-    if (process.argv.indexOf('-h') > -1) {
-      console.log("Usage: " + process.argv[1] + " [n]")
-      process.exit(0)
-    }
-  
-    count = process.argv[2]
+  if (args.includes("-n") || args.includes("--num")) {
+    const option = args.findIndex((option) => option === "-n" || option === "--num")
+    count = args[option + 1]
   }
   
   while(count--) {
-    //const result = flip_coin_cycles(); console.log(result[0] + ", " + result[1])
-    //console.log(get_random_byte())
-    console.log(get_random_hex())
+    if (args.includes("-h") || args.includes("--help")) {
+      print_usage()
+      process.exit(0)
+    }
+    else if (args.includes("-2") || args.includes("--sha256"))
+      console.log(hash_bytes())
+    else if (args.includes("-b") || args.includes("--bytes"))
+      console.log(get_byte("sha256"))
+    else if (args.includes("-c") || args.includes("--coin"))
+      console.log(flip_coin())
+    else if (args.includes("-f") || args.includes("--fair"))
+      console.log(fair_bit())
+    else if (args.includes("-s") || args.includes("--spins")) {
+      const result = coin_spins()
+      console.log(result[0] + ", " + result[1])
+    }
+    else if (args.includes("-v") || args.includes("--neumann"))
+      console.log(get_byte("neumann"))
+    else if (args.includes("-x") || args.includes("--hex"))
+      console.log(fair_bytes())
+    else
+      console.log(hash_bytes())
   }
 }
